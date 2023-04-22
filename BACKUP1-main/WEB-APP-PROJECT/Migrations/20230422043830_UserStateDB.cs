3c8d4fcd1d5f9e8f74a1f5879d3bd7a76e20e4aa
Tanapan1 <64010323@kmitl.ac.th>
2023-04-22 11:47:27 +0700

    22_4_2566_11_47

---
+++ BACKUP1-main/WEB-APP-PROJECT/Migrations/20230422043830_UserStateDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_APP_PROJECT.Migrations
+{
+    public partial class UserStateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserStates");
+        }
+    }
+}
---
+++ BACKUP1-main/WEB-APP-PROJECT/Models/UserState.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WEB_APP_PROJECT.Models
+{
+    public class UserState
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string State { get; set; }
+    }
+}
